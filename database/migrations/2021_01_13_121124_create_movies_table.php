71254794d3814bd06f37ae7cb97ca5151189a4f4
koki-a <mk.eng1119@gmail.com>
2021-03-03 15:18:31 +0900

    modified migration file

--- database/migrations/2021_01_13_121124_create_movies_table.php
+++ database/migrations/2021_01_13_121124_create_movies_table.php
@@ -23,7 +23,7 @@ class CreateMoviesTable extends Migration
             $table->string('url');
             $table->string('comment')->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
